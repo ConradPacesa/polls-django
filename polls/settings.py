fe58a40f7b8a5167d4609e464476e9922b16e5fa
Conrad Pacesa <conradpacesa@gmail.com>
2017-08-21 18:48:56 -0400

    Modified settings.py

--- polls/settings.py
+++ polls/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5=)+r3a!=!ba-&m1^96#flb#@5b2iw2(ch4eqo#mbq@hppm$mw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.99.100']
 
 
 # Application definition
