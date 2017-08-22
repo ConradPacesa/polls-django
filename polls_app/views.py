1d11aa37431ffc1ca5d84761cab7e43720c08ed7
Conrad Pacesa <conradpacesa@gmail.com>
2017-08-21 21:25:43 -0400

    Created html templates

--- polls_app/views.py
+++ polls_app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse('This is our index page')
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_questions' :latest_questions}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    return HttpResponse("this is question %s" % question_id)
