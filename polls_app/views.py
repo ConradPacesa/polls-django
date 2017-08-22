ffe6c8cde44d96e820ed3b50d1a50e19ea355f7e
Conrad Pacesa <conradpacesa@gmail.com>
2017-08-21 22:16:15 -0400

    Created additional html templates

--- polls_app/views.py
+++ polls_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Question
 
@@ -9,4 +9,5 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("this is question %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question' :question})
